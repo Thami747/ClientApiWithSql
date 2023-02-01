5aa0791e334702fc936c4ec5e8f359fcc13b0047
Thami Nkosi <thami.nkosi@zyberfox.com>
2023-02-01 11:42:45 +0200

    Initial commit

---
+++ clientapi/clientapi/src/main/java/com/thamiprojects/clientapi/ClientapiApplication.java
@@ -0,0 +1,13 @@
+package com.thamiprojects.clientapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientapiApplication.class, args);
+	}
+
+}
---
+++ clientapi/clientapi/src/test/java/com/thamiprojects/clientapi/ClientapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thamiprojects.clientapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
